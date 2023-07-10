895fdea07925b9bb8fef8e72f704d3e6d9e3633b
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-07-10 08:38:46 +0530

    [Amit]Add : Regex Examples and defination

---
+++ Regax/Regax/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Regax
+{
+    public class Program
+    {
+        //A Regex (regular expression)is a parttern that used to check wheater a given strinng matches that pattern.
+        //
+        static void Main(string[] args)
+        {
+            RegexEx regexEx = new RegexEx();
+            regexEx.MyRegex();
+        }
+    }
+}
---
+++ Regax/Regax/RegexEx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regax
+{
+    
+    public class RegexEx
+    {
+        static string pattern = "^cen$";
+        //^ It indicate string start.
+        //$ it will indicate the end of the string.
+        public void MyRegex()
+        {
+            //we have created object of class.
+            //we have pass a parameter pattern in given class constrcyor.
+            Regex regax = new Regex(pattern);
+            if (regax.IsMatch("Amit"))
+            {
+                Console.WriteLine("String is match the pattern");
+            }
+            else
+            {
+                Console.WriteLine("String is dose not match the pattern");
+            }
+        }
+    }
+}
